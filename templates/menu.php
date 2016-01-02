3b83908a54481027e3f386be675f9bb4d946d5ce
Bruno W. Bueno <brunowillianbueno@gmail.com>
2016-01-01 23:17:31 -0200

    Alteração na largura do menu

--- templates/menu.php
+++ templates/menu.php
@@ -6,30 +6,33 @@
  * Time: 18:14
  */?>
 <br>
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="../index.php"><img alt="PHP" class="img-responsive" src="../img/menu.png"></a>
-        </div>
+<div class="col-md-1"></div>
+<div class="col-md-10">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../index.php"><img alt="PHP" class="img-responsive" src="../img/menu.png"></a>
+            </div>
 
-        <div class="collapse navbar-collapse" id="nav-menu">
-            <ul class="nav navbar-nav">
-                <li><a href="../index.php"><i class="fa fa-shopping-bag fa-lg"></i> Produtos</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li class="text-center"><a href="#"><i class="fa fa-power-off"></i> Sair</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="nav-menu">
+                <ul class="nav navbar-nav">
+                    <li><a href="../index.php"><i class="fa fa-shopping-bag fa-lg"></i> Produtos</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li class="text-center"><a href="#"><i class="fa fa-power-off"></i> Sair</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
